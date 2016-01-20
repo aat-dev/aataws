96652f7a9e64490f31771233b0820b76a8fba8a2
dvdtoth <davee@davee.hu>
2016-01-20 08:12:23 +0000

    Added S3Connector

---
+++ src/S3Connector.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AAT\AWS;
+
+use Aws\S3\S3Client;
+
+/**
+ * Class S3Connector
+ * @package AAT\AWS
+ */
+class S3Connector extends AWS {
+
+  private $config;
+  private $client;
+
+  /**
+   * constructor for S3Client
+   * @param $credentials
+   */
+  function __construct($credentials) {
+    $this->client = S3Client::factory($credentials);
+  }
+
+  /**
+   * List buckets
+   * @return \Aws\Result
+   */
+  function listBuckets() {
+    return $this->client->ListBuckets();
+  }
+
+  /**
+   * Copy object
+   * @param $src
+   * @param $dst
+   */
+  function copyObject($source, $destination_bucket, $destination_key, $acl) {
+    $object = [
+      // Source path with bucket eg: bucket/path/to/file.ext
+      'CopySource' => $source,
+      // Destination bucket
+      'Bucket' => $destination_bucket,
+      // Destination path/to/file.ext
+      'Key' => $destination_key,
+      // ACL
+      'ACL' => $acl
+    ];
+    $result = $this->client->copyObject($object);
+    return $result;
+  }
+
+  /**
+   * Delete object
+   * @param $bucket
+   * @param $key
+   * @return \Aws\Result|\Guzzle\Service\Resource\Model
+   */
+  function deleteObject($bucket, $key) {
+    $result = $this->client->deleteObject(['Bucket' => $bucket, 'Key' => $key]);
+    return $result;
+  }
+}
